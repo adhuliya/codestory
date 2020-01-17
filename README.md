34317835af31a4575b05db7ad01770ef1b3c7c30
adhuliya <dhuliya@cse.iitb.ac.in>
2020-01-17 23:53:11 +0530

    semi-automated-update

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # codestory
 
 Write stories in your code :)
+
+### Usage:
+
+    cd to/your/source/directory's/parent
+    codestory src    # assuming codestory is in the path
+    firefox codestory/document.html
+
+`document.html` is stored in the system generated `codestory/` folder
+which houses other essential files for the html file to work.
+
+### How to use?
+
+This package comes with a `sample` folder. Invoke `codestory` on the folder,
+
+    codestory sample
+    firefox codestory/document.html
+
+View the document and see the content of the files
+and observe which parts of the text get mapped to the html
+document and how do they look.
+(More comprehensive doc will come soon)
+
+
---
+++ sample/Hello.cpp
@@ -0,0 +1,77 @@
+//===- Hello.cpp - Example code from "Writing an LLVM Pass" ---------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements two versions of the LLVM "Hello World" pass described
+// in docs/WritingAnLLVMPass.html
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/ADT/Statistic.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+using namespace llvm;
+
+#define DEBUG_TYPE "hello"
+
+STATISTIC(HelloCounter, "Counts number of functions greeted");
+
+namespace {
+  // Hello - The first implementation, without getAnalysisUsage.
+  struct Hello : public FunctionPass {
+    static char ID; // Pass identification, replacement for typeid
+    Hello() : FunctionPass(ID) {}
+
+    bool runOnFunction(Function &F) override {
+      ++HelloCounter;
+      errs() << "Hello: ";
+      errs().write_escaped(F.getName()) << '\n';
+      return false;
+    }
+  };
+}
+
+char Hello::ID = 0;
+//>>BLOCK(HOW_TO_REGISTER_PASS_LEGACY.10)START
+//>>How is a pass registered? (Legacy PM)
+//>>We create an object of the `RegisterPass` class template,
+//>>from the file defining our pass.
+//>>The template is instantiated to the `Hello` pass we want
+//>>registered here. The two arguments to the constructer
+//>>are the:
+//>>1. First: A command line argument needed to invoke the pass (`-hello` in this case)
+//>>2. Second: A short meaningful description for the users of the pass.
+//>>
+//>>The name `X` is itself not used anywhere (although the object created is).
+static RegisterPass<Hello> X("hello", "Hello World Pass");
+//>>BLOCK(HOW_TO_REGISTER_PASS_LEGACY.10)END
+
+namespace {
+  // Hello2 - The second implementation with getAnalysisUsage implemented.
+  struct Hello2 : public FunctionPass {
+    static char ID; // Pass identification, replacement for typeid
+    Hello2() : FunctionPass(ID) {}
+
+    bool runOnFunction(Function &F) override {
+      ++HelloCounter;
+      errs() << "Hello: ";
+      errs().write_escaped(F.getName()) << '\n';
+      return false;
+    }
+
+    // We don't modify the program, so we preserve all analyses.
+    void getAnalysisUsage(AnalysisUsage &AU) const override {
+      AU.setPreservesAll();
+    }
+  };
+}
+
+char Hello2::ID = 0;
+static RegisterPass<Hello2>
+Y("hello2", "Hello World Pass (with getAnalysisUsage implemented)");
